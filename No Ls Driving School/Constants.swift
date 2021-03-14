f69c25243550a6a5a7ea858416691950a0f17382
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-03-14 10:34:33 +0530

    constants added

--- No Ls Driving School/Constants.swift
+++ No Ls Driving School/Constants.swift
@@ -8,6 +8,16 @@ struct Asserts {
     static let reviews = UIImage(named: "navrev")!
     static let contacts = UIImage(named: "navcont")!
     
+    // Common
+    static let navHeader = UIImage(named: "newtop")!
+    
     // Home
     static let header = UIImage(named: "main")!
+    static let theoryTest = UIImage(named: "ttpro")!
+}
+
+
+// MARK: Fonts
+struct Fonts {
+    static let avenirNext = "Avenir Next"
 }
