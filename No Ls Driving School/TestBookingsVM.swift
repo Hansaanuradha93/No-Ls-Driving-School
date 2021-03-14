1d4da92118d8ba85e42c2d76d07b369340cca366
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-03-14 13:12:47 +0530

    TestBookingVM added

---
+++ No Ls Driving School/TestBookingsVM.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+final class TestBookingsVM {
+    
+    // MARK: Properties
+    lazy var webPagesArray = [
+        WebPage(name: "Book Theory Test", urlString: "https://www.gov.uk/book-theory-test"),
+        WebPage(name: "Change Theory Test", urlString: "https://www.gov.uk/change-theory-test"),
+        WebPage(name: "Book Practical Test", urlString: "https://www.gov.uk/book-driving-test"),
+        WebPage(name: "Change Practical Test", urlString: "https://www.gov.uk/change-driving-test"),
+    ]
+}
