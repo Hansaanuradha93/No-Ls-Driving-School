92e9f9321f53527292ff50b25bbdd0f146b34f53
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-03-14 10:34:09 +0530

    LELabel added and LSButton tweaked

--- No Ls Driving School/LSButton.swift
+++ No Ls Driving School/LSButton.swift
@@ -11,10 +11,15 @@ class LSButton: UIButton {
     required init?(coder: NSCoder) {  fatalError() }
     
     
-    convenience init(backgroundColor: UIColor = .white, title: String = "", titleColor: UIColor = .black) {
+    convenience init(backgroundColor: UIColor = AppColor.darkestGray, title: String = "", titleColor: UIColor = AppColor.darkestGray, fontSize: CGFloat = 16) {
         self.init(frame: .zero)
+        let traits = [UIFontDescriptor.TraitKey.weight: UIFont.Weight.bold]
+        var descriptor = UIFontDescriptor(fontAttributes: [UIFontDescriptor.AttributeName.family: Fonts.avenirNext])
+        descriptor = descriptor.addingAttributes([UIFontDescriptor.AttributeName.traits: traits])
+        
         self.setTitle(title, for: .normal)
         self.setTitleColor(titleColor, for: .normal)
         self.backgroundColor = backgroundColor
+        self.titleLabel?.font = UIFont(descriptor: descriptor, size: fontSize)
     }
 }
